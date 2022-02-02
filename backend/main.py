64eb54baa734cb485d4a4f159c6ce2ad3dab45a3
khansohil <khansohil530@gmail.com>
2022-02-02 13:34:27 +0530

    main file with app instance. Does basic CRUD requests. CORS using CORSMiddleware from fastapi

---
+++ backend/main.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+from database import(
+    fetch_one_contact,
+    fetch_all_contacts,
+    create_contact,
+    update_contact,
+    remove_contact,
+)
+from model import Contact
+
+app = FastAPI()
+
+
+# List of allowed origins
+origins = ["https://localhost:3000"]
+
+# Adding is as 'middleware' to fast api app. Look at fastapi documentation for CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Ping": "Pong"}
+
+
+@app.get("/api/contact")
+async def get_contact():
+    response = await fetch_all_contacts()
+    return response
+
+
+@app.get("/api/contact{name}", response_model=Contact)
+async def get_contact_by_id(name):
+    response = await fetch_one_contact(name)
+    if response:
+        return response
+
+    raise HTTPException(404, f"There is no contact with name: {name}")
+
+
+@app.post("/api/contact", response_model=Contact, status_code=status.HTTP_201_CREATED)
+async def post_contact(contact: Contact):
+    response = await create_contact(contact.dict())
+    if response:
+        return response
+    raise HTTPException(400, "Bad request")
+
+
+@app.put("/api/contact{name}", response_model=Contact)
+async def put_contact(name: str, contact: str):
+    response = await update_contact(name, contact)
+    if response:
+        return response
+
+    raise HTTPException(404, f"Contact with name: {name} does not exist")
+
+
+@app.delete("/api/contact{name}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_contact(name):
+    response = await remove_contact(name)
+    if response:
+        return "Successfully delete contact"
+
+    raise HTTPException(404, f"Contact with name: {name} does not exist")
