17ef98b02820f1e6805496a7571511c339b1c562
khansohil <khansohil530@gmail.com>
2022-02-06 20:36:46 +0530

    Connect bootstrap and fontawesome

--- frontend/contact-manager/src/index.js
+++ frontend/contact-manager/src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Fontawesome icons*
+
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+
+// BootStrap 5.1*
+
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
 
 ReactDOM.render(
   <React.StrictMode>
