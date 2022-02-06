2900dfd45fef3844e29b7e53f86eff5230ef2ab4
khansohil <khansohil530@gmail.com>
2022-02-06 20:47:20 +0530

    Test bootstrap and fa working

--- frontend/contact-manager/src/App.js
+++ frontend/contact-manager/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h2>App Component</h2>
+      <button className='btn btn-success me-1'><i className='fa fa-home'></i> Click</button>
     </div>
   );
 }
