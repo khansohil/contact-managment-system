0a4bdcdfa5b280f4d2e0e3b0c8841f2bf19e31dd
khansohil <khansohil530@gmail.com>
2022-02-07 10:34:00 +0530

    Setup files for components

---
+++ frontend/contact-manager/src/components/contacts/EditContact/EditContact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+let EditContact = () => {
+    return(
+        <React.Fragment>
+            <h2>EditContact
+            
+            </h2>
+        </React.Fragment>
+    )
+}
+
+export default EditContact
